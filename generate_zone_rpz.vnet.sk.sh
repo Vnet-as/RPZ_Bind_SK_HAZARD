bfe5e89e9ca19409a6065e7bd87555baf21f0b03
PVi1 <peter.vilhan@vnet.eu>
2020-02-27 10:13:26 +0100

    curl - disable output buffering to prevent error 23

--- generate_zone_rpz.vnet.sk.sh
+++ generate_zone_rpz.vnet.sk.sh
@@ -24,7 +24,7 @@ REDIRECT_DST="rpz.vnet.sk."
 SN=$(date +"%Y%m%d%H")
 
 # Stiahnutie dokumentu
-LINK=$(curl -k -so - https://www.urhh.sk/web/guest/zoznam-zakazanych-sidel | sed  s/"> <"/">\n<"/g | grep 'Zoznam zakázaných webových sídiel k.*pdf' -m1 | cut -d'"' -f2)
+LINK=$(curl -N -k -so - https://www.urhh.sk/web/guest/zoznam-zakazanych-sidel | sed  s/"> <"/">\n<"/g | grep 'Zoznam zakázaných webových sídiel k.*pdf' -m1 | cut -d'"' -f2)
 if ! /usr/bin/wget -t 1 -nd -r -l 1 --ignore-case -A pdf -O "${PDF_NAME}" -q "${LINK}"; then
 	echo "Nepodarilo sa stiahnut dokument s bloknutymi webmi z linku ${LINK}!" >&2
 	exit 1
